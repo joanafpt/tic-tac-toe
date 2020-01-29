67052af194201491ef80f4c5e6d3abb494b319fe
joanafpt <joanajteixeira@outlook.com>
2020-01-29 11:32:42 +0000

    gh pages

---
+++ precache-manifest.3aa188fe9dd169c1288584a64e815563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b940cf49c99aaf4a7ae21dfbad6b827e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c04746add17269883f0d",
+    "url": "/static/css/main.a55da2d0.chunk.css"
+  },
+  {
+    "revision": "9bcc8c8b373c4b556edc",
+    "url": "/static/js/2.560fdbda.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.560fdbda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c04746add17269883f0d",
+    "url": "/static/js/main.1f7bf59a.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
